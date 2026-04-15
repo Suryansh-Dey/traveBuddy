4f845a620b26e21f1b1b8002d199157a1e401fb8
Suryansh Dey <deysuryansh@gmail.com>
2026-04-15 20:38:24 +0530

    using render backend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -17,7 +17,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: "/api/proxy/:path*",
-        destination: "http://127.0.0.1:8000/:path*",
+        destination: "https://travebuddy.onrender.com/:path*",
       },
     ];
   },
